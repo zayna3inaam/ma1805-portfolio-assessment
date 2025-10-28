542d3c41947843a5864cd5589ae915db4e58a29b
zayna3inaam <zayna3inaam@gmail.com>
2025-10-28 13:28:55 +0000

    Add files via upload

--- 01.drawing/sketch.js
+++ 01.drawing/sketch.js
@@ -1,27 +1,58 @@
 
-
-let person = 55;
-
-let cold = false;
-let home;
-console. log(home)
-
+let happy=true;//track whether the emoji is smiling or frowning.
 
 function setup() {
   createCanvas(400, 400);
-  background(255, 10, 100)
-  let pet = "cat"
 }
 
 function draw() {
-  background(220);
- // circle(50, 50, 50)
- 
-  circle(100, 100, 100)
-  rect(200, 200, 100)
-  person= person+0.3
-  console.log(person)
-  // The text
+  background(100);
+  
+  //rectangles
 
+  fill(255, 215, 0);
+  rect(40, 40, 150, 300);
+  fill(173,216,230, 200);
+  rect(60, 60, 130, 250);
 
+  
+  //face
+  fill(255, 255, 0);
+ circle(200, 200, 200); 
+  
+  
+  //eyes
+  fill(255,255,255)
+  circle(150, 180, 50);
+  circle(250, 180, 50);
+  
+  fill(0,0,0)
+  circle(150, 180, 10);
+  circle(250, 180, 10);
+  
+  //mouth
+  noFill();
+stroke(0);
+strokeWeight(5);
+  
+  if(happy) {
+arc(200, 220, 100, 50, 0, PI);// smile
+  }else {
+    
+arc(200, 250, 100, 50, PI, 0);// frown
+  }
+    
 }
+  
+// this function must be outside draw()
+  
+
+function mousePressed() {
+  happy= !happy; // toggle between smile and frown on click
+
+ 
+  
+  
+}
+
+// emoji of "self reflcetion",users can choose whether to have the emoji smile or frown depending on the context.
