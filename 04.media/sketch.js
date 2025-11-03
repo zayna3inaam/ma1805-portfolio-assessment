2650e979bab7fb54eb7434182e7874980a082cc0
zayna3inaam <zayna3inaam@gmail.com>
2025-11-03 14:30:35 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,5 +1,6 @@
 let img;
 let song;
+let capture;
 
 // Load the image.
 function preload() {
@@ -9,12 +10,16 @@ function preload() {
 
 function setup(){
   createCanvas(windowWidth,windowHeight)
+  capture= createCapture(VIDEO);
+  capture.size(320, 240);
+  capture.hide();
 }
 
 function draw(){
   background(200)
   // Draw the image.
-  image(img, 0, 0, width, 500);
+  image(img, 0, 0, windowWidth, 500);
+  image(capture,0, 0, 320, 240);
 }
 
 function mouseClicked(){
