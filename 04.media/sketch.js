0118645a5c23e87b4391426e79ab35c5e2c92806
zayna3inaam <zayna3inaam@gmail.com>
2025-11-03 13:34:07 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,42 +1,30 @@
-let img; // Declare variable 'img'.
+let img;
+let song;
 
-let hum;
-let capture;
-let video;
-
-
+// Load the image.
 function preload() {
-  img = loadImage('Antenna.jpg'); // Load image
-   hum = loadSound("humm.wav");
-  video=createVideo('TheSea.mov')
+  img = loadImage('kitten.jpg');
+  song = loadSound("piano.wav");
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  capture.hide();
-   capture.hide();
+function setup(){
+  createCanvas(windowWidth,windowHeight)
 }
 
-function draw() {
-  // Display image at actual size at x/y (0,0)
-  image(img, 0, 0);
-  // Displays half size image at x/y (0, 100)
-  //image(img, 0, 100, img.width/2, img.height/2);
-
- // image(capture,200,200, 320, 240)
-  image(video, 100, 0, 350, 240);
-
+function draw(){
+  background(200)
+  // Draw the image.
+  image(img, 0, 0, width, 500);
 }
 
 function mouseClicked(){
-  hum.loop();
-  hum.amp(0.2); 
-  video.loop(0.2);
-
   if (getAudioContext().state !== 'running') {
     getAudioContext().resume();
   }
+  // play or loop the song
+  if(!song.isPlaying()){
+    song.loop();
+    song.amp(0.2);
+  }
 }
+  
